b9e833c348402e4336b8dd7c05e9d5c62c8fce6c
aleksejfilippov <ahtamar909@gmail.com>
2018-03-19 01:29:53 +0300

    ONLY first horizontal combo is working

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
-import com.sun.org.apache.regexp.internal.RE;
+
 import javafx.application.Application;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseButton;
@@ -9,9 +9,12 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
-import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 //
 ////
@@ -196,42 +199,109 @@ import javafx.stage.Stage;
 //////}
 
 public class Main extends Application {
-
+    private boolean clickedX = true;
+    private boolean playable = true;
+    private List<Combo> combos = new ArrayList<>();
+    private Cube[][] board = new Cube[3][3];
     // тут прописывается правильный путь до картинки
     String crest = getClass().getResource("i_091.png").toExternalForm();
     String circle = getClass().getResource("circle-thin.png").toExternalForm();
 
-    @Override
-    public void start(Stage stage) throws Exception{
-        Pane pane  = new Pane();
-        for (int i = 0; i < 3; i++){
-            for (int j = 0; j <3; j++){
+    private Parent createContent() {
+        Pane pane = new Pane();
+
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 Cube cube = new Cube();
                 cube.setTranslateX(i * 100);
                 cube.setTranslateY(j * 100);
                 pane.getChildren().addAll(cube);
+                board[i][j] = cube;
 
             }
         }
-        Scene scene = new Scene(pane, 300, 300);
-        stage.setScene(scene);
+        //получим три выигрышные строки
+        for (int y = 0; y < 3; y++) {
+            combos.add((new Combo(board[0][y], board[1][y], board[2][y])));
+        }
+        //теперь три выигрышных столбца
+        for (int x = 0; x < 3; x++) {
+            combos.add(new Combo(board[x][0], board[x][1], board[x][2]));
+        }
+        //и диагональные
+        combos.add(new Combo(board[0][0], board[1][1], board[2][2]));
+        combos.add(new Combo(board[2][0], board[1][1], board[0][2]));
+
+        return pane;
+    }
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(createContent(), 600, 600));
         stage.show();
     }
-    private class Cube extends StackPane{
+
+    public void checkState() {
+        for (Combo combo : combos) {
+            if (combo.isComplete())
+                playable = false;
+            break;
+        }
+    }
+
+    public class Combo {
+        private Cube[] cubes;
+
+        public Combo(Cube... cubes) {
+            this.cubes = cubes;
+        }
+
+        public boolean isComplete() {
+            if (cubes[0].getValue() == " ")
+                return false;
+            else
+                return cubes[0].getValue() == cubes[1].getValue() && cubes[1].getValue() == cubes[2].getValue();
+        }
+    }
+
+    private class Cube extends StackPane {
+        private Image imageX = new Image(crest);
+        private Image imageO = new Image(circle);
+        private String value = " ";
+
         public Cube() {
             Rectangle rectangle = new Rectangle(100, 100, Color.WHITE);
             rectangle.setStroke(Color.BLACK);
             setAlignment(Pos.CENTER);
             getChildren().addAll(rectangle);
             setOnMouseClicked(e -> {
-                if(e.getButton() == MouseButton.PRIMARY){
-
-                }
-                else {
-                    System.out.println("prim");
+                System.out.println(playable + this.value);
+                if (!playable)
+                    return;
+                if (e.getButton() == MouseButton.PRIMARY) {
+                    if (!clickedX)
+                        return;
+                    rectangle.setFill(new ImagePattern(imageX));
+                    this.value = "x";
+                    clickedX = false;
+                    checkState();
+                } else {
+                    if (clickedX)
+                        return;
+                    rectangle.setFill(new ImagePattern(imageO));
+                    this.value = "o";
+                    clickedX = true;
+                    checkState();
                 }
                 e.consume();
             });
+        }
+
+
+        public String getValue() {
+            return this.value;
         }
     }
 
